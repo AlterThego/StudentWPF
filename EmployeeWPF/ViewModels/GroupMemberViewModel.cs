ac827bdc61b9c5c4f3490a09e01b0499ff6e7f8d
DeucesOUT1 <113009566+DeucesOUT1@users.noreply.github.com>
2023-12-16 22:23:41 +0800

    Add files via upload
    Group

---
+++ EmployeeWPF/ViewModels/GroupMemberViewModel.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentWPF.ViewModels
+{
+    public class GroupMemberViewModel : INotifyPropertyChanged
+    {
+        private string _name;
+        private string _role;
+
+        public string Name
+        {
+            get { return _name; }
+            set
+            {
+                if (_name != value)
+                {
+                    _name = value;
+                    OnPropertyChanged(nameof(Name));
+                }
+            }
+        }
+
+        public string Role
+        {
+            get { return _role; }
+            set
+            {
+                if (_role != value)
+                {
+                    _role = value;
+                    OnPropertyChanged(nameof(Role));
+                }
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
